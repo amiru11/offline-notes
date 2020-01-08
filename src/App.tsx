cf750a6c2b8a7e25183f9a8f8d132a9df02bef2b
Saemii <wrfg12@gmail.com>
2020-01-08 15:08:37 +0900

    Create globalStyle.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
+import GlobalStyle from "./styles/globalStyle";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    </>
   );
 };
 

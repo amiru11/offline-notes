e36ed168ef2c8684b6c86cc15ebf2287579cd53a
Saemii <wrfg12@gmail.com>
2020-01-14 11:46:57 +0900

    Component 명 변경

--- src/components/Note/Add.tsx
+++ src/components/Note/Add.tsx
@@ -5,19 +5,15 @@ import { CREATE_NOTE } from "../../mutations/note";
 import { INote } from "../types";
 import Editor from "../Editor";
 
-function AddNote(): JSX.Element {
+function NoteAdd(): JSX.Element {
   const history = useHistory();
   const [createNote] = useMutation<{ createNote: INote }>(CREATE_NOTE);
 
   const onSave = async (title: string, content: string): Promise<void> => {
     try {
-      console.log("onSave");
-      console.log("title", title);
-      console.log("content", content);
       const note = await createNote({ variables: { title, content } });
       if (note) {
-        console.log(note);
-        // history.push("/");
+        history.push("/");
       } else {
         throw new Error("Error!");
       }
@@ -29,4 +25,4 @@ function AddNote(): JSX.Element {
   return <Editor onSave={onSave} />;
 }
 
-export default AddNote;
+export default NoteAdd;

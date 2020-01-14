790e0a24819e5049aa609f9838e4f1b33fb35d85
Saemii <wrfg12@gmail.com>
2020-01-14 14:35:43 +0900

    add font-family in body

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,54 +1,55 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  html, body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-  }
-  :root {
-      --greyColor: #A2A19E;
-      --blackColor: #373630;
-      --pinkColor: #ff9999;
-  }
-  body{
-      color:var(--blackColor);
-  }
-  a {
-      color:inherit;
-      text-decoration:none;
-  }
-  div{
-      margin:0;
-  }
-  input,
-  textarea{
-      appearance:none;
-      border:none;
-      background-color:transparent;
-      resize:none;
-      &::placeholder {
-          color: #E7E7E6;
-      }
-      &:focus,
-      &:active{
-          outline:none;
-      }
-  }
-  .markdown a{
-      text-decoration:underline;
-  }
-  button{
-      appearance:none;
-      border:none;
-      background-color:transparent;
-      font-weight:600;
-      font-size:15px;
-      cursor:pointer;
-      border:2.5px solid var(--blackColor);
-      &:focus,
-      &:active{
-          outline:none
-      }
-  }
+html, body {
+margin: 0;
+padding: 0;
+height: 100%;
+}
+:root {
+    --greyColor: #A2A19E;
+    --blackColor: #373630;
+    --pinkColor: #ff9999;
+}
+body{
+    font-family: "Apple SD Gothic Neo", AppleSDGothicNeo, "Noto Sans CJK KR", NanumGothic, "Malgun Gothic", "맑은 고딕", 돋움, dotum, arial, sans-serif;
+    color:var(--blackColor);
+}
+a {
+    color:inherit;
+    text-decoration:none;
+}
+div{
+    margin:0;
+}
+input,
+textarea{
+    appearance:none;
+    border:none;
+    background-color:transparent;
+    resize:none;
+    &::placeholder {
+        color: #E7E7E6;
+    }
+    &:focus,
+    &:active{
+        outline:none;
+    }
+}
+.markdown a{
+    text-decoration:underline;
+}
+button{
+    appearance:none;
+    border:none;
+    background-color:transparent;
+    font-weight:600;
+    font-size:15px;
+    cursor:pointer;
+    border:2.5px solid var(--blackColor);
+    &:focus,
+    &:active{
+        outline:none
+    }
+}
 `;

057857094d4c1291e9bf3b306e414701a9f78bed
Saemii <wrfg12@gmail.com>
2020-01-14 11:46:06 +0900

    using notes query with fragment

--- src/queries/note.ts
+++ src/queries/note.ts
@@ -4,11 +4,10 @@ import { NOTE_FRAGMENT } from "../fragments/note";
 export const GET_NOTES = gql`
   {
     notes @client {
-      id
-      title
-      content
+      ...NoteParts
     }
   }
+  ${NOTE_FRAGMENT}
 `;
 
 export const GET_NOTE = gql`
